ba00b5a19a5eef3f995e2b5a94dbd432a6a4faed
Yonghui Zhao <zhaoyonghui@gmail.com>
2013-05-27 21:11:13 +0800

    Change artifact id for deploy

--- project/build/KafkaProject.scala
+++ project/build/KafkaProject.scala
@@ -60,7 +60,7 @@ class KafkaProject(info: ProjectInfo) extends ParentProject(info) with IdeaProje
       </dependency>
     </dependencies>
 
-    override def artifactID = "kafka"
+    override def artifactID = "core-kafka_2.8.0"
     override def filterScalaJars = false
 
     // build the executable jar's classpath.
